d541e5be632a2118789cb4c5eee00f3448400a41
Mateo Baca <mateobacas07@gmail.com>
2025-07-30 12:59:08 -0500

    feat: Creacion de rutas para controlador queryTemplate

---
+++ src/routes/queryTemplate.routes.ts
@@ -0,0 +1,41 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import QueryTemplateController from '../controllers/querytemplate.controller.js';
+
+const QueryTemplateRouter = Router();
+
+QueryTemplateRouter.get(
+  '/',
+  (req: Request, res: Response, next: NextFunction) => {
+    QueryTemplateController.getAllQueryTemplates(req, res, next);
+  }
+);
+
+QueryTemplateRouter.get(
+  '/:id',
+  (req: Request, res: Response, next: NextFunction) => {
+    QueryTemplateController.getQueryTemplateById(req, res, next);
+  }
+);
+
+QueryTemplateRouter.post(
+  '/',
+  (req: Request, res: Response, next: NextFunction) => {
+    QueryTemplateController.createQueryTemplate(req, res, next);
+  }
+);
+
+QueryTemplateRouter.put(
+  '/:id',
+  (req: Request, res: Response, next: NextFunction) => {
+    QueryTemplateController.updateQueryTemplate(req, res, next);
+  }
+);
+
+QueryTemplateRouter.delete(
+  '/:id',
+  (req: Request, res: Response, next: NextFunction) => {
+    QueryTemplateController.deleteQueryTemplate(req, res, next);
+  }
+);
+
+export default QueryTemplateRouter;
