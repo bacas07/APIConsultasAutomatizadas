40ca7c4bd38adba17e2e5b05322b7bf6766c1376
Mateo Baca <mateobacas07@gmail.com>
2025-07-30 21:56:26 -0500

    feat: Creacion de rutas para autenticacion

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,20 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import AuthController from '../controllers/auth.controller.js';
+
+const AuthRouter = Router();
+
+AuthRouter.post(
+  '/register',
+  (req: Request, res: Response, next: NextFunction) => {
+    AuthController.register(req, res, next);
+  }
+);
+
+AuthRouter.post(
+  '/login',
+  (req: Request, res: Response, next: NextFunction) => {
+    AuthController.login(req, res, next);
+  }
+);
+
+export default AuthRouter;
